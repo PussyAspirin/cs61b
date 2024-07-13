d41cd25faa712ffb9ba3a30d772f8abd720bd632
PussyAspirin <bull3hitking@gmail.com>
2024-07-13 20:53:40 +0800

    3. modify dcatenate

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -83,6 +83,7 @@ public class IntList {
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
         IntList ptr = A;
+        if (ptr == null) return B;
         while (ptr.rest != null) {
             ptr = ptr.rest;
         }
